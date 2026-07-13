641d3fa871e23caed0cfc256254cd8387a4b5da6
Calum <6991398+0x63616c@users.noreply.github.com>
2026-07-13 11:49:27 -0700

    chore(autopush): snapshot 2026-07-13 11:49:27 PDT

--- nvim/lua/plugins/snacks-explorer.lua
+++ nvim/lua/plugins/snacks-explorer.lua
@@ -1,9 +1,14 @@
 -- Hide Godot .uid sidecar files in the explorer and all pickers (files, grep, ...).
+-- Show dotfiles and gitignored files everywhere, but always carve out .git and node_modules.
 return {
   "folke/snacks.nvim",
   opts = {
     picker = {
-      exclude = { "*.uid" },
+      exclude = { "*.uid", "**/.git/*", "**/node_modules/*" },
+      sources = {
+        explorer = { hidden = true, ignored = true },
+        files = { hidden = true, ignored = true },
+      },
     },
   },
 }
